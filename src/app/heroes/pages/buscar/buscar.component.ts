bc7676ad0731c790e474971aa2d696e33fdc0a9b
JohanLeonardoIbarra <johanleonardois@ufps.edu.co>
2022-06-15 16:25:00 -0500

    200

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -10,11 +11,20 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent implements OnInit {
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
 
   buscando() {
     this.heroesService
-      .getHeroes()
+      .getSugerencias(this.termino)
       .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+    this.heroesService
+      .getHeroe(heroe.id!)
+      .subscribe((heroe) => (this.heroeSeleccionado = heroe));
   }
 
   constructor(private heroesService: HeroesService) {}
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -16,8 +16,11 @@ export class HeroesService {
   getHeroe(id: string): Observable<Heroe> {
     return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
-  getSugerencias(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes/${''}`);
+  getSugerencias(query: string): Observable<Heroe[]> {
+    const params: HttpParams = new HttpParams()
+      .set('_limit', 5)
+      .set('q', query);
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`, { params });
   }
   constructor(private http: HttpClient) {}
 }
